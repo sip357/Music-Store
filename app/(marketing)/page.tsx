32d7d07e1cc4d527a19a62284d111fa4960b1761
sip357 <somto2345@gmail.com>
2024-06-09 17:35:29 -0600

    Added store.tsx

--- app/(marketing)/page.tsx
+++
@@ -1,8 +0,0 @@
-export default async function MarketPage() {
-    return (
-      <div>
-        <h1>Discover a variety of beats at unbeatable prices</h1>
-      </div>
-    );
-  }
-  
--- app/pages/store.tsx
+++ app/pages/store.tsx
@@ -3,9 +3,11 @@ import styles from '../styles/Home.module.css';
 
 export default function Home() {
   return (
+    <Layout>
       <div className={styles.container}>
-        <h1>Where instrumentals meet talent</h1>
-        <p>Discover your sound</p>
+        <h1>Delve into my world of Music</h1>
+        <p>Discover a variety of beats at unbeatable prices</p>
       </div>
+    </Layout>
   );
 }
