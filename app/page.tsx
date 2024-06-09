956ad6a59d6c6ad8aaa4895f97b9567d8460a58d
sip357 <somto2345@gmail.com>
2024-06-09 17:41:32 -0600

    Update

--- app/page.tsx
+++ app/page.tsx
@@ -2,12 +2,14 @@ import Layout from "./components/layout";
 import App from "next/app";
 import './styles/globals.css';
 
-export default function Home() {
+export default function def() {
   return (
+    <Layout>
     <div>
       <h1>Where instrumentals meet talent</h1>
       <p>Discover your sound</p>
     </div>
+    </Layout>
   );
 }
 
