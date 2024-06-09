1de0c2b010565d660fc294bbdac0197a7a60f914
sip357 <somto2345@gmail.com>
2024-06-09 17:39:17 -0600

    Update

--- app/pages/index.tsx
+++ app/pages/index.tsx
@@ -3,9 +3,11 @@ import styles from '../styles/Home.module.css';
 
 export default function Home() {
   return (
+    <Layout>
       <div className={styles.container}>
         <h1>Where instrumentals meet talent</h1>
         <p>Discover your sound</p>
       </div>
+    </Layout>
   );
 }
