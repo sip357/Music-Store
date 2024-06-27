d4e5f23370dd40f99955093c4fd4a0b81328cd0d
sip357 <somto2345@gmail.com>
2024-06-27 15:22:23 +0100

    Update

--- app/components/NavBar.tsx
+++ app/components/NavBar.tsx
@@ -25,7 +25,7 @@ const NavBar: React.FC = () => {
           <Link href='/'>Home</Link>
         </li>
         <li>
-          <Link href="./store" scroll={false}>Shop</Link>
+          <Link href="/store">Shop</Link>
         </li>
         <li>
           <Link href="#about" scroll={true} className={styles.dropdown}>
