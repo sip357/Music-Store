c2321a95e5fc968f1edce4e743d5b69cc8f506fc
sip357 <somto2345@gmail.com>
2024-06-28 14:50:46 +0100

    Update

--- app/store/page.tsx
+++ app/store/page.tsx
@@ -1,11 +1,12 @@
 import React from 'react';
 import Layout from "../components/layout";
 import styles from '../styles/Store.module.css';
+import Link from 'next/link';
 
 const Store: React.FC = () => {
   return (
     <Layout>
-      <section className={styles.body}>
+      <section className={`${styles.body} ${styles.fourpi}`}>
         <div className={`${styles.fourpi} ${styles.container}`}>
         <span className={styles.onep}><button className={styles.button_34} role="button">Instrumentals</button></span>
         <span className={styles.onep}><button className={styles.button_34} role="button">Soundkits</button></span>
@@ -15,6 +16,7 @@ const Store: React.FC = () => {
       </div>
       <div className={styles.productsContainer}>
         <div className={styles.product}>
+          <img src="studio.jpg" alt="picture" className={styles.productImage}/>
           <p className={styles.productName}>Beat 1</p>
         </div>
       </div>
