e26be0fe4b5ef70b35a433913aacfa56f7a07c26
sip357 <somto2345@gmail.com>
2024-07-01 11:07:23 +0100

    Trying to add the MongoDB database

--- app/store/page.tsx
+++ app/store/page.tsx
@@ -1,25 +1,26 @@
 import React from 'react';
 import Layout from "../components/layout";
 import styles from '../styles/Store.module.css';
+import ShoppingCartIcon from '../components/shopping_cart';
 
 const Store: React.FC = () => {
   return (
     <Layout>
       <section className={`${styles.body} ${styles.fourpi}`}>
         <div className={`${styles.fourpi} ${styles.container}`}>
-        <span className={styles.onep}><button className={styles.button_34} role="button">Instrumentals</button></span>
-        <span className={styles.onep}><button className={styles.button_34} role="button">Soundkits</button></span>
-      </div>
-      <div className={`${styles.div} ${styles.container} ${styles.fourpi}`}>
-        <p className={styles.divP}>Browse through our selection of beats and sound kits.</p>
-      </div>
-      <div className={styles.productsContainer}>
-        <div className={styles.product}>
-          <img src="studio.jpg" alt="picture" className={styles.productImage}/>
-          <a href="/" className={`${styles.productName} ${styles.a}`}>Beat 1</a>
-          <i className="fal fa-shopping-cart">Add</i>
+          <span className={styles.onep}><button className={styles.button_34} role="button">Instrumentals</button></span>
+          <span className={styles.onep}><button className={styles.button_34} role="button">Soundkits</button></span>
         </div>
-      </div>
+        <div className={`${styles.div} ${styles.container} ${styles.fourpi}`}>
+          <p className={styles.divP}>Browse through our selection of beats and sound kits.</p>
+        </div>
+        <div className={styles.productsContainer}>
+          <div className={styles.product}>
+            <img src="studio.jpg" alt="picture" className={styles.productImage}/>
+            <a href="/" className={`${styles.productName} ${styles.a}`}>Beat 1</a>
+            <ShoppingCartIcon />
+          </div>
+        </div>
       </section>
     </Layout>
   );
