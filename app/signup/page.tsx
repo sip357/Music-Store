cb7cfcb463b6f395c0af592fcb511f285883e1b9
sip357 <somto2345@gmail.com>
2024-07-13 19:07:04 +0100

    update

--- app/signup/page.tsx
+++ app/signup/page.tsx
@@ -1,15 +1,20 @@
 import React from "react";
 import Layout from "../components/layout";
 import styles from "./Signup.module.css";
-import ShoppingCartIcon from "../components/searchBox";
+import "../styles/globals.css";
 
 const SignUp: React.FC = () => {
     return (
       <Layout>
-        <div className={styles.fivep}>
-          <div className={styles.gg_play_button}></div>
-          <ShoppingCartIcon />
+        <div className={styles.div}>
+          <form action="">
+            <div className={`${styles.twopb}`}>
+              <input type="text" placeholder="First Name" className={`${styles.inputField}`}/>
+            </div>
+            <button className={`${styles.button_3}`}>Submit</button>
+          </form>
         </div>
+        
       </Layout>
     );
   };
