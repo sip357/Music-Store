94704e04971f88df4306504459f0a367485d682f
sip357 <somto2345@gmail.com>
2024-07-15 12:01:08 +0100

    Added EencType to form

--- app/signup/page.tsx
+++ app/signup/page.tsx
@@ -7,11 +7,16 @@ const SignUp: React.FC = () => {
     return (
       <Layout>
         <div className={`${styles.div} ${styles.mg_auto}`}>
-          <form action="">
+          <form action="process-form" method="post" encType="multipart/form-data">
             <div className={`${styles.twopb}`}>
-              <input type="text" placeholder="Enter a name" className={`${styles.inputField}`}/>
+              <label htmlFor="Name">Name</label>
+              <input type="text" id="name" placeholder="Enter a name" className={`${styles.inputField}`}/>
             </div>
-            <div className={`${styles.twopb} ${styles.mg_auto}`}>
+            <div className={`${styles.twopb}`}>
+              <label htmlFor="Email">Email</label>
+              <input type="text" id="email" placeholder="Enter your email address" className={`${styles.inputField}`}/>
+            </div>
+            <div className={`${styles.twopb} ${styles.text_align_center}`}>
               <button className={`${styles.button_3}`}>Submit</button>
             </div>
           </form>
