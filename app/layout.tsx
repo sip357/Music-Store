448996177586dad0628f69f4bcefe81460b5fb0f
sip357 <somto2345@gmail.com>
2024-07-18 10:58:41 +0100

    Installed mongodb

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,6 +1,5 @@
-import NavBar from "./NavBar"
-import Head from "next/head"
 
+import Head from "next/head"
 
 export default function RootLayout({
   children,
@@ -10,12 +9,6 @@ export default function RootLayout({
   return (
     <html lang="en">
       <body>
-        <Head>
-          <title>SIP's Music Store</title>
-          <meta name="description" content="A website for selling instrumentals by SIP" />
-          <link rel="icon" href="/favicon.ico" />
-        </Head>
-        <NavBar />
         <main>{children}</main>
         <footer>
           <p>&copy; 2023 SIP</p>
--- app/page.tsx
+++ app/page.tsx
@@ -1,11 +1,26 @@
+'use client';
 import Layout from "./layout";
 import App from "next/app";
 
+
 export default function Home() {
+  const callApi = async () =>{
+    await fetch('/api/users/route', {
+      method: "GET",
+      headers: {
+        'Content-Type': 'application/json'
+      },
+      body: JSON.stringify({})
+    })
+      .then(response => response.json())
+      .then(data => console.log(data))
+      .catch(error => console.error('Error:', error));
+  }
   return (
     <div>
       <h1>Welcome to my Music Store</h1>
       <p>Discover a variety of beats at unbeatable prices</p>
+      <button onClick={callApi}>Call API</button>
     </div>
   );
 }
