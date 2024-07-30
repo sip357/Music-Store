2cb68714430d57d0515736425d7ba1786e274a19
sip357 <somto2345@gmail.com>
2024-07-30 16:47:22 +0100

    made productpage responsive

--- app/store/[id]/page.tsx
+++ app/store/[id]/page.tsx
@@ -1,6 +1,7 @@
 import React from "react"
 import Layout from "../../components/layout"
 import styles from "../[id]/detail.module.css";
+import "../../../public/studio.jpg"
 
 async function fetchBeatData(id) {
   const res = await fetch(`http://localhost:3000/api/products/${id}`);
@@ -29,38 +30,35 @@ export default async function ProductPage({ params }) {
   
   return (
     <Layout>
-      <div className={`${styles.detailContainer}`}>
-        <div className={`${styles.title}`}>
-          {beat.Title}
+        <div className={`${styles.imageContainer} ${styles.padding_top}`}>
+          <img src="\studio.jpg" alt="picture" className={`${styles.productImage}`}/>
+          <div className="onep">
+            <select name="Pick Lease" id="SelectLease" className={`${styles.select}`}>
+              <option value="First Form">First Form</option>
+              <option value="Second Form">Second Form</option>
+              <option value="Third Form">Third Form</option>
+              <option value="Exclusive">Exclusive</option>
+            </select>
+          </div>
+        </div> 
+        <div className={`${styles.detailContainer}`}>
+          <div className={`${styles.title}`}>
+            {beat.Title}
+          </div>
+          <div className={`${styles.details}`}>
+            {beat.BPM} BPM
+          </div>
+          <div className={`${styles.details} ${styles.tagContainer}`}>
+            {beat.Tags.map((tag) => 
+            <span className={`${styles.onep}`} key={tag.id}><button className={`${styles.button_34}`}>{tag}</button></span>
+            )}
+          </div>
+          <span className={styles.addToCart}>
+            <button className={styles.addToCart}>
+              Add to Cart
+            </button>
+          </span>
         </div>
-        <div className={`${styles.details}`}>
-          BPM: {beat.BPM}
-        </div>
-        <div className={`${styles.details} ${styles.tagContainer}`}>
-          {beat.Tags.map((tag) => 
-          <span className={`${styles.onep}`} key={tag.id}><button className={`${styles.button_34}`}>{tag}</button></span>
-          )}
-        </div>
-        <select name="Pick Lease" id="SelectLease">
-          <option value="First Form">First Form</option>
-          <option value="Second Form">Second Form</option>
-          <option value="Third Form">Third Form</option>
-          <option value="Exclusive">Exclusive</option>
-        </select>
-        <span className={styles.addToCart}>
-          <button className={styles.addToCart}>
-            Add to Cart
-          </button>
-        </span>
-      </div>
-      
-      <div className={`${styles.imageContainer} ${styles.padding_top}`}>
-        Image
-      </div>
-      <div>
-        Title: {beat.Title}
-      </div>
-      
     </Layout>
   );
 }
