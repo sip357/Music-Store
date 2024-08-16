5e3056c388a81a15aafd6ae691b23d3e4d1879bb
sip357 <somto2345@gmail.com>
2024-08-16 14:21:03 +0100

    Updated regex validation for emails and password

--- app/signup/page.tsx
+++ app/signup/page.tsx
@@ -6,16 +6,18 @@ import Layout from "../components/layout";
 import styles from "./Signup.module.css";
 import PurpleButton from "../components/purpleButton";
 import "../styles/globals.css";
+import ReCAPTCHA from "react-google-recaptcha";
 
 function ValidateEmail(email: string): boolean {
-  const validRegex = /^[a-zA-Z0-9.!#$%&'*+/=?^_`{|}~-]+@[a-zA-Z0-9-]+(?:\.[a-zA-Z0-9-]+)*$/;
+  const validRegex = /^[a-zA-Z0-9.!#$%&'*+/=?^_`{|}~-]+@[a-zA-Z0-9-]+\.[a-zA-Z0-9-]+$/;
   return validRegex.test(email);
 }
 
-// function ValidatePassword(password: string): boolean{
-//   const validRegex = /^[a-zA-Z0-9!#$%&'*+/=?^_`{|}~-]+$/;
-//   return validRegex.test(password);
-// }
+
+function ValidatePassword(password: string): boolean{
+  const validRegex = /^(?=.*?[A-Z])(?=.*?[a-z])(?=.*?[0-9])(?=.*?[#?!@$()%^&*.-{}_]).{8,}$/;
+  return validRegex.test(password);
+}
 
 const Home: React.FC = () => {
   const [name, setName] = useState<string>('');
@@ -26,6 +28,7 @@ const Home: React.FC = () => {
   const [emailError, setEmailError] = useState<string>('');
   const [passwordError, setPasswordError] = useState<string>('');  
   const [isButtonDisabled, setIsButtonDisabled] = useState<boolean>(true);
+  const [recaptchaToken, setRecaptchaToken] = useState(null);
   const router = useRouter();
 
   useEffect(() => {
@@ -45,19 +48,18 @@ const Home: React.FC = () => {
       setEmailError(""); // Clear email error if validation passes
     }
   
-    if (password.length <= 10) {
-      setPasswordError("Password must be longer than 10 characters");
+    if(!ValidatePassword(password)){
+      setPasswordError("Must be at least 8 characters and contain at least 1 of each: a-z, A-Z, 0-9, #?!@$()_%^{}&*.-")
       isValid = false;
-    } else {
-      setPasswordError(""); // Clear password error if validation passes
-    }
+      } else {
+        setPasswordError(""); // Clear password error if validation passes
+      }
   
     setIsButtonDisabled(!isValid); // Enable button if all validations pass
     setError(""); // Clear general error if specific errors are handled
   
   }, [name, email, password]);
   
-
   const createUser = async () => {
     try {
       const response = await fetch('/api/userAPI', {
@@ -98,24 +100,29 @@ async function userExists(): Promise<boolean> {
 
 const handleSubmit = async (event: React.FormEvent<HTMLFormElement>) => {
   event.preventDefault();
+
+  if (!recaptchaToken) {
+    setError('*Please complete the reCAPTCHA.');
+    return;
+  }
   
   if (name.length <= 3 || !name) {
-    setError('Name is required and must be longer than 3 characters');
+    setNameError('*Name is required and must be longer than 3 characters');
     return;
   }
   
   if (!email) {
-    setError('Email is required to create a user');
+    setEmailError('*Email is required to create a user');
     return;
   }
 
   if (!ValidateEmail(email)) {
-    setError('Invalid email address');
+    setEmailError('*Invalid email address');
     return;
   }
 
-  if (!password) {
-    setError('Enter a password');
+  if (!ValidatePassword(password)) {
+    setPasswordError('*Enter a password');
     return;
   }
 
@@ -130,59 +137,84 @@ const handleSubmit = async (event: React.FormEvent<HTMLFormElement>) => {
   createUser(); // Call your createUser function
 };
 
+  const handleBackClick = () =>{
+    router.back(); //Navigate to the previous page
+  }
+
+  //Confirms script loaded
+  const asyncScriptOnLoad = () => {
+    console.log('Google recaptcha loaded just fine')
+  }
+
+  const handleRecaptchaChange = (token) => {
+    setRecaptchaToken(token);
+  };
+
   return (
     // <Layout>
       <html>
         <body>
+          <span className={styles.arrow} onClick={handleBackClick} style={{ cursor: 'pointer' }}>
+            &#8592;
+          </span>
           <div className={`${styles.div} ${styles.mg_auto} ${styles.padding_top}`}>
-        <form onSubmit={handleSubmit} className={`${styles.twopb}`}>
-          <div className={`${styles.twopb}`}>
-            <label htmlFor="name" className={`${styles.required}`}>Name</label>
-            <input
-              type="text"
-              id="name"
-              placeholder="Enter a name"
-              value={name}
-              onChange={(e) => setName(e.target.value)}
-              className={`${styles.inputField}`}
-              required
-            />
-            {nameError && <span style={{ color: 'red', padding:0 }}>{nameError}</span>}
+            <form onSubmit={handleSubmit} className={`${styles.twopb}`}>
+              <div className={`${styles.twopb}`}>
+                <label htmlFor="name" className={`${styles.required}`}>Name</label>
+                <input
+                  type="text"
+                  id="name"
+                  placeholder="Enter a name"
+                  value={name}
+                  onChange={(e) => setName(e.target.value)}
+                  className={`${styles.inputField}`}
+                  required
+                />
+                {nameError && <span style={{ color: 'red', padding:0 }}>{nameError}</span>}
+              </div>
+              
+              <div className={`${styles.twopb}`}>
+                <label htmlFor="email" className={`${styles.required}`}>Email</label>
+                <input
+                  type="email"
+                  id="email"
+                  placeholder="Enter your email address"
+                  value={email}
+                  onChange={(e) => setEmail(e.target.value)}
+                  className={`${styles.inputField}`}
+                  required
+                />
+                {emailError && <span style={{ color: 'red', padding:0 }}>{emailError}</span>}
+              </div>
+              
+              <div className={`${styles.twopb}`}>
+                <label htmlFor="password" className={`${styles.required}`}>Password</label>
+                <input
+                  type="password"
+                  id="password"
+                  placeholder="Enter your password"
+                  value={password}
+                  onChange={(e) => setPassword(e.target.value)}
+                  className={`${styles.inputField}`}
+                  required
+                />
+                {passwordError && <span style={{ color: 'red', padding:0 }}>{passwordError}</span>}
+              </div>
+              <ReCAPTCHA
+                sitekey="6Ld0KygqAAAAAP_3HnKNPU4DVliG50nFTqUyTmT3"
+                onChange={handleRecaptchaChange}
+                asyncScriptOnLoad={asyncScriptOnLoad}
+              />
+              {/* Added the styles here because they would only be implemented once */}
+              <p style={{color : 'white', fontWeight: "bold"}} className={`${styles.text_align_center}`}>
+                Already have an account? <a href="/login" className={`${styles.loginLink}`}>Login</a>
+              </p>
+              {error && <p style={{ color: 'red', fontWeight: "bold"}}>{error}</p>}
+              <div className={`${styles.onepb} ${styles.text_align_center}`}>
+                <PurpleButton param1="Sign up" param2={isButtonDisabled}/>
+              </div>
+            </form>
           </div>
-          
-          <div className={`${styles.twopb}`}>
-            <label htmlFor="email" className={`${styles.required}`}>Email</label>
-            <input
-              type="email"
-              id="email"
-              placeholder="Enter your email address"
-              value={email}
-              onChange={(e) => setEmail(e.target.value)}
-              className={`${styles.inputField}`}
-              required
-            />
-            {emailError && <span style={{ color: 'red', padding:0 }}>{emailError}</span>}
-          </div>
-          
-          <div className={`${styles.twopb}`}>
-            <label htmlFor="password" className={`${styles.required}`}>Password</label>
-            <input
-              type="password"
-              id="password"
-              placeholder="Enter your password"
-              value={password}
-              onChange={(e) => setPassword(e.target.value)}
-              className={`${styles.inputField}`}
-              required
-            />
-            {passwordError && <span style={{ color: 'red', padding:0 }}>{passwordError}</span>}
-          </div>
-          {error && <p style={{ color: 'red' }}>{error}</p>}
-          <div className={`${styles.twopb} ${styles.text_align_center}`}>
-            <PurpleButton param1="Sign up" param2={isButtonDisabled}/>
-          </div>
-        </form>
-      </div>
         </body>
       </html>
       
