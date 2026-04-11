bb434d7840bb11ce2b3b297a34c4d9a686846cfb
sip357 <somto2345@gmail.com>
2026-04-11 17:14:54 -0600

    Added declarations file

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,6 +1,6 @@
 import type { Metadata } from "next";
 import localFont from "next/font/local";
-import "./globals.css";
+import "./styles/globals.css";
 import NavBar from "./components/nav/navbar";
 import Footer from "./components/footer";
 import AudioPlayer from "./components/audioPlayer";
